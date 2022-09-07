e18eda60ce75f717d48fcddce63d74920f49bb33
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-07 13:01:51 +0100

    Renamed Home.js to Article.js

---
+++ src/Article.js
@@ -0,0 +1,18 @@
+import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
+const Article = () => {
+  const { data, isPending, error } = useFetch(" http://localhost:8000/blogs");
+  return (
+    <div className="Article">
+      {error && (
+        <div>
+          <br />
+          <h2>{error}</h2>
+        </div>
+      )}
+      {isPending && <div> loading...</div>}
+      {data && <Bloglist blogs={data} title="all blogs" />}
+    </div>
+  );
+};
+export default Article;
