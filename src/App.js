4241bbce6c396475d322e89f00cf16fea75de397
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-07 18:28:09 +0100

    turns out my stuff did not commit before smh.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./header";
 import Create from "./Create";
 import Article from "./Article";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import firebaseApp from "./firebaseConfig";
 
 function App() {
   return (
