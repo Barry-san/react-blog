a3b0db37431b843ba50709f50d14e2fcbc2c1e4d
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-07 18:40:27 +0100

    updated the create component

--- src/Create.js
+++ src/Create.js
@@ -1,67 +1,20 @@
 import { useState } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { getFirestore } from "firebase/firestore";
-import firebaseApp from "./firebaseConfig";
 
 const Create = () => {
-  const db = getFirestore(firebaseApp);
-=======
-// import { History } from "history";
-
-const Create = () => {
->>>>>>> 52a8660 (added a page to creat Blogs)
-=======
-import { getFirestore } from "firebase/firestore";
-import firebaseApp from "./firebaseConfig";
-
-const Create = () => {
-  const db = getFirestore(firebaseApp);
->>>>>>> 6c856b9 (trying to commit again smh.)
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const blog = { title, content, author: "Yoshi" };
 
   const handleSubmit = (e) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
     e.preventDefault();
-    const blog = { title, content, author: "Yoshi" };
-    console.log(db.collection("blogs"));
-    db.collection("blogs")
-      .add(blog)
-      .then((ref) => console.log("document reference is : " + ref.id));
     console.log(blog);
-=======
-    const blog = { title, content, author: "Yoshi" };
-    console.log(blog);
-    e.preventDefault();
->>>>>>> 52a8660 (added a page to creat Blogs)
-=======
-    e.preventDefault();
-    const blog = { title, content, author: "Yoshi" };
-    console.log(db.collection("blogs"));
-    db.collection("blogs")
-      .add(blog)
-      .then((ref) => console.log("document reference is : " + ref.id));
-    console.log(blog);
->>>>>>> 6c856b9 (trying to commit again smh.)
     fetch("http://localhost:8000/blogs", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog),
-<<<<<<< HEAD
-<<<<<<< HEAD
     }).then((res) => {
       console.log(res);
     });
-=======
-    }).then();
->>>>>>> 52a8660 (added a page to creat Blogs)
-=======
-    }).then((res) => {
-      console.log(res);
-    });
->>>>>>> 3f19df7 (updated json files and created new component.)
   };
   return (
     <div className="create">
