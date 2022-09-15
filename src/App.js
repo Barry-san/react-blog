088a3a54c7f556b14ca155b6134aaa53c4aef029
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-15 18:22:50 +0100

    added more routes to the app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Article from "./components/Article";
 import NotFound from "./components/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/Signin";
+import Register from "./components/Register";
 import BlogDetails from "./components/BlogDetails";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Article />}></Route>
           <Route path="/create" element={<Create />}></Route>
           <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/register" element={<Register />}></Route>
           <Route path="/blog/:id" element={<BlogDetails />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
