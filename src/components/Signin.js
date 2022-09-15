d3377f85b820d8388bc9b46148c98e12784648cb
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-15 18:20:12 +0100

    made changes to the signin component

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,9 +1,44 @@
-const SignIn = () => {
+import { app } from "../firebaseConfig";
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const SingIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInput = (event) => {
+    let input = event.target.value;
+    setEmail(input);
+  };
+
+  const handleInput2 = (event) => {
+    let input = event.target.value;
+    setPassword(input);
+  };
+
+  const auth = getAuth();
+  const handleClick = () => {
+    signInWithEmailAndPassword(auth, email, password).then((response) => {
+      console.log(response);
+    });
+  };
   return (
-    <div className="signin">
-      <button>Sign in with google.</button>
+    <div className="login">
+      <input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={handleInput}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={handleInput2}
+      />
+      <button onClick={handleClick}>login</button>
     </div>
   );
 };
 
-export default SignIn;
+export default SingIn;
